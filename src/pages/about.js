01618a0eb86e33c65354f8560f4bfc57d9039add
Jalen <jalensmith126@gmail.com>
2024-01-24 23:52:07 -0500

    bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,10 +21,13 @@ const AboutPage = () => (
         <p className="fg text-black">
           Jalen Smith is a travel film creative and nomadic storyteller. Hailing from southern California, he transferred to Yale upon graduating from community college in 2022 and is currently completeing an interdiscplinary major of political science and film.
            With an extensive background in advocacy, Jalen seeks to do meaningful work in visual storytelling and ecotourism. Through documentary, adventure filmmaking, and cinematic outdoor videography, he hopes to travel, capture the natural world, and explore
-          the intersection between entertainment and the environment. He is currently writing his senior thesis on the energy crisis in South Africa and the government corruption of Eskom, under the supervision of South African author
+          the intersection between entertainment and the environment. 
+          
+          <br/><br/>He is currently writing his senior thesis on the South African energy crisis and the corruption of its major utility company, Eskom, under the supervision of South African author
           and Yale and Oxford Law Professor Jonny Steinberg.
 
           <br /><br />Outside of being an adventurer, Jalen is also an avid skateboarder, writer, frequent moviegoer, and die-hard comic book fan who can tell you one too many Marvel movie easter-eggs.
+          <br /><br /> Photo by Cullen Welch
         </p>
       </div>
     </div>
