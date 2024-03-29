98e0076637e971886012ce6d798d6f158e7d1a31
Jalen <jalensmith126@gmail.com>
2024-03-29 16:54:46 -0400

    edit

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -45,7 +45,7 @@ const IndexPage = () => {
           <span className="image">
             <img src={pic1} alt="Stills" />
           </span>
-          <Link to="/au">
+          <Link to="/stills">
             <div className="content">
               <h2>STILLS</h2>
             </div>
