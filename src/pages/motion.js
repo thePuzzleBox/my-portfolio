79e950d2bdf6359ec5909a7ba037cfa5d8d5438d
Jalen <jalensmith126@gmail.com>
2024-06-14 01:32:55 -0400

    vids

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -3,11 +3,11 @@ import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-import satellite from '../assets/images/fulls/vids/satellite.mp4';
-
 const VideoPage = () => {
   const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
   const videoRefs = {
+    wn: useRef(null),
+    skate: useRef(null),
     satellite: useRef(null),
   };
 
@@ -26,36 +26,39 @@ const VideoPage = () => {
       <section style={{ minHeight: '100vh' }}>
         <div className="app" style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
           <div className="video-container" style={{ width: '50%', maxWidth: '1000px', paddingBottom: '50px', paddingTop: '150px', textAlign: 'center' }}>
-            <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%', background: '#000', marginBottom: '5px' }}>
-              <iframe
-                title="wn"
-                src="https://www.youtube.com/embed/bH25f59aO4I?rel=0&modestbranding=1&showinfo=0&controls=1&autoplay=0"
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
+            <iframe
+              width="100%"
+              height="500"
+              src="https://www.youtube.com/embed/bH25f59aO4I?enablejsapi=1&modestbranding=1&rel=0&showinfo=0"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="WN Video"
+            ></iframe>
           </div>
 
           <div className="video-container" style={{ width: '50%', maxWidth: '1000px', paddingBottom: '50px', textAlign: 'center' }}>
-            <div style={{ position: 'relative', paddingBottom: '56.25%', height: 0, overflow: 'hidden', maxWidth: '100%', background: '#000', marginBottom: '5px' }}>
-              <iframe
-                title="skate"
-                src="https://www.youtube.com/embed/t6AaEDmw9ak?rel=0&modestbranding=1&showinfo=0&controls=1&autoplay=0"
-                style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-              ></iframe>
-            </div>
+            <iframe
+              width="100%"
+              height="500"
+              src="https://www.youtube.com/embed/t6AaEDmw9ak?enablejsapi=1&modestbranding=1&rel=0&showinfo=0"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Skate Video"
+            ></iframe>
           </div>
 
           <div className="video-container" style={{ width: '50%', maxWidth: '1000px', paddingBottom: '50px', textAlign: 'center' }}>
-            <video ref={videoRefs.satellite} controls onPlay={() => handleVideoPlay('satellite')}>
-              <source src={satellite} type="video/mp4" />
-              Your browser does not support the video tag.
-            </video>
+            <iframe
+              width="100%"
+              height="500"
+              src="https://www.youtube.com/embed/7Lo8HUedzUs?enablejsapi=1&modestbranding=1&rel=0&showinfo=0"
+              frameBorder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+              title="Satellite Video"
+            ></iframe>
           </div>
         </div>
       </section>
