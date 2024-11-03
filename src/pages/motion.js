fdf165955ea6f9a8d6242582a6e4f6d368c33d80
Jalen <jalensmith126@gmail.com>
2024-11-03 13:44:18 -0800

    vids

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -28,6 +28,38 @@ const VideoPage = () => {
           <div className="video-container" style={{ paddingTop: '160px'}}>
             <div className="video-wrapper">
               <iframe
+                ref={videoRefs.tnc}
+                width="100%"
+                height="500"
+                src="https://youtube.com/embed/A1xf2RrvUOk?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="TNC"
+              ></iframe>
+            </div>
+          </div>
+
+
+          <div className="video-container">
+            <div className="video-wrapper">
+              <iframe
+                ref={videoRefs.au}
+                width="100%"
+                height="500"
+                src="https://youtube.com/embed/rpTR5hea4GE?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="WNT"
+              ></iframe>
+            </div>
+          </div>
+            
+            
+          <div className="video-container">
+            <div className="video-wrapper">
+              <iframe
                 ref={videoRefs.wn}
                 width="100%"
                 height="500"
