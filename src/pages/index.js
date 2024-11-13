b8ee99ce8cfbf6480b3838e8798a0a29c1557793
Jalen <jalensmith126@gmail.com>
2024-11-13 13:32:34 -0800

    edits

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Subscribe from '../components/Subscribe';
 
-import reel from '../assets/images/landing.mp4';
+import reel from '../assets/images/landing1.mp4';
 
 const IndexPage = () => (
   <Layout>
--- src/pages/motion.js
+++ src/pages/motion.js
@@ -41,7 +41,7 @@ const VideoPage = () => {
           </div>
 
 
-          <div className="video-container">
+          {/*<div className="video-container">
             <div className="video-wrapper">
             <iframe
                 ref={videoRefs.tnc}
@@ -54,7 +54,7 @@ const VideoPage = () => {
                 title="TNC"
               ></iframe>
             </div>
-          </div>
+          </div> */}
             
             
           <div className="video-container">
