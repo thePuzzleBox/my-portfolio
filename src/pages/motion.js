43cf9b041933432b95d8b083f5110b8c6a3b35dd
Jalen <jalensmith126@gmail.com>
2024-12-14 19:48:16 -0800

    upd

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -26,6 +26,23 @@ const VideoPage = () => {
       <section style={{ minHeight: '100vh' }}>
         <div className="app" style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
           <div className="video-container" style={{ paddingTop: '160px'}}>
+            
+          <div className="video-container">
+            <div className="video-wrapper">
+            <iframe
+                ref={videoRefs.tnc}
+                width="100%"
+                height="500"
+                src="https://youtube.com/embed/A1xf2RrvUOk?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="TNC"
+              ></iframe>
+            </div>
+          </div>
+            
+            
             <div className="video-wrapper">
             <iframe
                 ref={videoRefs.au}
@@ -55,23 +72,7 @@ const VideoPage = () => {
             </div>
           </div>
 
-          <div className="video-container">
-            <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.tnc}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/A1xf2RrvUOk?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="TNC"
-              ></iframe>
-            </div>
-          </div>
-            
-            
-
+          
 
           {/*<div className="video-container">
             <div className="video-wrapper">
