e703dbb93696431ec3fbf406f14828eeaeb0a12c
Jalen <jalensmith126@gmail.com>
2024-12-19 21:49:16 -0800

    upd

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -27,38 +27,9 @@ const VideoPage = () => {
         <div className="app" style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
           
           
-          <div className="video-container" style={{ paddingTop: '160px'}}>  
-            <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.tnc}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/A1xf2RrvUOk?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="TNC"
-              ></iframe>
-            </div>
-          </div>
-
-          <div className="video-container">
-          <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.reg}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/231F8qevHLg?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="regrow"
-              ></iframe>
-            </div>
-          </div>
             
             
-          <div className="video-container">
+          <div className="video-container" style={{ paddingTop: '160px'}}>
             <div className="video-wrapper">
             <iframe
                 ref={videoRefs.au}
@@ -84,6 +55,38 @@ const VideoPage = () => {
                 allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="WN"
+              ></iframe>
+            </div>
+          </div>
+
+
+          <div className="video-container">
+          <div className="video-wrapper">
+            <iframe
+                ref={videoRefs.grow}
+                width="100%"
+                height="500"
+                src="https://youtube.com/embed/231F8qevHLg?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="regrow"
+              ></iframe>
+            </div>
+          </div>
+
+
+          <div className="video-container">  
+            <div className="video-wrapper">
+            <iframe
+                ref={videoRefs.tnc}
+                width="100%"
+                height="500"
+                src="https://youtube.com/embed/A1xf2RrvUOk?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="TNC"
               ></iframe>
             </div>
           </div>
