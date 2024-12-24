58b28cea14e4fa8cdd07de2f0c15effea275c053
Jalen <jalensmith126@gmail.com>
2024-12-23 16:57:01 -0800

    about up

--- src/pages/about.js
+++ src/pages/about.js
@@ -20,7 +20,7 @@ const AboutPage = () => (
           Jalen Smith is a nomadic filmmaker and visual storyteller from Los Angeles, CA. He graduated studying film and political science at Yale College. Upon transferring from community college with 
           an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. He aims to travel, capture the natural world, share its stories, and through his work, prompt others to ask themselves what it means to be a part of it.
           
-          <br/><br/>Jalen dedicated his senior thesis to the ongoing energy crisis in South Africa, detailing the corruption and downfall of its major utility company, Eskom. In it, he recounts his experience having to live with the effects of a country's politics in the wild.
+          <br/><br/>Jalen dedicated his senior thesis to the ongoing energy crisis in South Africa, detailing the corruption and downfall of its major utility company, Eskom. In it, he recounts his experience living in the wild with the effects of a country's politics.
 
           <br /><br />Outside of being an adventurer, Jalen is an avid skateboarder, writer, undercover pianist, and urban forester who recently ventured into the world of freediving. Those closest to him would describe him as incredibly spontaneous and a modern jack-of-all-trades. As he believes his greatest skill is his ability to learn new skills.
         </p>
