fb39d55ee72125c46886e2f217e75a0f02b2063c
Jalen <jalensmith126@gmail.com>
2025-01-08 15:07:58 -0800

    edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,7 +15,7 @@ const AboutPage = () => (
         <img src={portrait} className="img-fluid" alt="" />
       </div>
       <div className="col-lg-8 mx-auto">
-        <h2 className="fg text-black mb-4 text-center">About</h2>
+        <h2 className="fg text-black mb-4 text-center">{/*possible title */}</h2>
         <p className="fg text-black">
           Jalen Smith is a nomadic filmmaker and visual storyteller from Los Angeles, CA. He graduated studying film and political science at Yale College. Upon transferring from community college with 
           an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. He aims to travel, capture the natural world, share its stories, and through his work, prompt others to ask themselves what it means to be a part of it.
