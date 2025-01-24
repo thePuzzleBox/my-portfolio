cc983a1cb054f89ce33253b680f28492c4241510
Jalen <jalensmith126@gmail.com>
2025-01-24 03:15:08 -0800

    edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,9 +18,9 @@ const AboutPage = () => (
         <h2 className="fg text-black mb-4 text-center">{/*possible title */}</h2>
         <p className="fg text-black">
           Jalen Smith is a nomadic filmmaker and visual storyteller from Los Angeles, CA. He graduated studying film and political science at Yale College. Upon transferring from community college with 
-          an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. He aims to travel, capture the natural world, share its stories, and through his work, prompt others to ask themselves what it means to be a part of it.
+          an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. He aims to travel, capture the natural world, share its stories of conservation, and through his work, prompt others to ask themselves what it means to be a part of it.
           
-          <br/><br/>Jalen dedicated his senior thesis to the ongoing energy crisis in South Africa, detailing the corruption and downfall of its major utility company, Eskom. In it, he recounts his experience living in the wild and having to adapt to the effects of a country's politics.
+          <br/><br/>Jalen dedicated his senior thesis to the ongoing energy crisis in South Africa after living there, detailing the corruption and downfall of its major utility company, Eskom. In it, he recounts his experience living in the wild and having to adapt to the man-made effects of a country's politics.
 
           <br /><br />Outside of being an adventurer, Jalen is an avid skateboarder, writer, undercover pianist, and urban forester who recently ventured into the world of freediving. Those closest to him would describe him as incredibly spontaneous and a modern jack-of-all-trades. As he believes his greatest skill is his ability to learn new skills.
         </p>
