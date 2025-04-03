49ce85325d25bd92778fcf6cb0f3e0a164a9d304
Jalen <jalensmith126@gmail.com>
2025-04-03 12:10:27 -0700

    edit

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -41,6 +41,21 @@ const VideoPage = () => {
             </div>
             </div>
 
+            <div className="video-container">
+            <div className="video-wrapper">
+              <iframe
+                ref={videoRefs.skate}
+                width="100%"
+                height="500"
+                src="https://www.youtube.com/embed/t6AaEDmw9ak?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="skate"
+              ></iframe>
+            </div>
+          </div>
+
 
             <div className="video-container">  
             <div className="video-wrapper">
@@ -138,20 +153,6 @@ const VideoPage = () => {
           
 
           
-        <div className="video-container">
-            <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.skate}
-                width="100%"
-                height="500"
-                src="https://www.youtube.com/embed/t6AaEDmw9ak?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="skate"
-              ></iframe>
-            </div>
-          </div>*/
 
           <div className="video-container">
             <div className="video-wrapper">
