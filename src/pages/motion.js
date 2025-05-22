264cae3f41b32b16a43f2651370e8ef655298fd3
Jalen <jalensmith126@gmail.com>
2025-05-22 14:37:54 -0400

    edit

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -29,23 +29,6 @@ const VideoPage = () => {
         <div className="video-container" style={{ paddingTop: '160px'}}>
             <div className="video-wrapper">
             <iframe
-                ref={videoRefs.au}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/rpTR5hea4GE?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="WNT"
-              ></iframe>
-            </div>
-            </div>
-        
-        
-        
-        <div className="video-container">
-        <div className="video-wrapper">
-            <iframe
                 ref={videoRefs.grow}
                 width="100%"
                 height="500"
@@ -57,7 +40,23 @@ const VideoPage = () => {
               ></iframe>
             </div>
             </div>
-
+        
+        
+        
+        <div className="video-container">
+        <div className="video-wrapper">
+            <iframe
+                ref={videoRefs.au}
+                width="100%"
+                height="500"
+                src="https://youtube.com/embed/rpTR5hea4GE?controls=1&rel=0&modestbranding=1"
+                frameBorder="0"
+                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="WNT"
+              ></iframe>
+            </div>
+            </div>
 
 
             <div className="video-container">  
