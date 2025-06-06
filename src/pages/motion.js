b609149ab92caa15007cf67584fc8c00b7f8b7a0
Jalen <jalensmith126@gmail.com>
2025-06-05 19:12:06 -0700

    wild

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -32,7 +32,7 @@ const VideoPage = () => {
                 ref={videoRefs.yasc}
                 width="100%"
                 height="500"
-                src="https://youtube.com/embed/jf5DybyWT6w?controls=1&rel=0&modestbranding=1"
+                src="https://youtube.com/embed/C3iJDd7vCAs?controls=1&rel=0&modestbranding=1"
                 frameBorder="0"
                 allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
