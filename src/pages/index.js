09e09dc13959c0945827e6281455d7cfe8d50838
Jalen <jalensmith126@gmail.com>
2025-09-23 21:05:13 -0700

    video

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Ending from '../components/Ending';
 
-import reel from '../assets/images/landing1.mp4';
+import reel from '../assets/images/landing.mp4';
 
 const IndexPage = () => (
   <Layout>
