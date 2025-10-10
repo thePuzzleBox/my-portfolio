ce87382fcbd3aa8f354269e1584483c87d843990
Jalen <jalensmith126@gmail.com>
2025-10-09 18:04:39 -0700

    good

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,11 +18,11 @@ const AboutPage = () => (
         <h2 className="fg text-black mb-4 text-center">{/*possible title */}</h2>
         <p className="fg text-black">
           Jalen Smith is a nomadic filmmaker and conservation storyteller from Los Angeles, CA. He graduated studying film and political science at Yale College. Upon transferring from community college with 
-          an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. Since then, his commitment to conservation, reforestation, and storytelling has led him to work and film in communities worldwide.
+          an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. Through filming wildlife and teaching high school students how to plant trees, he learned he had a passion for nature and for sharing it. 
           
           
-          <br/><br/> Having lived in some of the most remote places on Earth, Jalen found his passion for nature and for sharing it. From sailing on expedition across Indonesia to planting trees across South America, he has found himself
-          in the presence of exceptionally diverse fauna, people, ecosystems, and their alternative ways of life. As a result, he aims to share this wealth and knowledge of the natural world, and through its stories of preservation,
+          <br/><br/> Since then, Jalen's commitment to reforestation and conservation storytelling has led him to work and film in communities worldwide, in some of the most remote regions on Earth. From sailing on expedition across Indonesia to planting trees across South America, he has found himself
+          in the presence of exceptionally diverse fauna, people, ecosystems, and their alternative ways of life. As a result, he aims to give back, share this wealth and knowledge of the natural world, and through its stories of preservation,
           he hopes others find their place in it too.
 
 
