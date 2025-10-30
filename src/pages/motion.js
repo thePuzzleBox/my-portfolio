b1ae224fe466ebc839963ef4caceca4f6b4d073a
Jalen <jalensmith126@gmail.com>
2025-10-30 01:02:16 -0700

    motion grid

--- src/pages/motion.js
+++ src/pages/motion.js
@@ -8,7 +8,15 @@ const VideoPage = () => {
   const videoRefs = {
     wn: useRef(null),
     skate: useRef(null),
-    satellite: useRef(null),
+    ox: useRef(null),
+    yasc: useRef(null),
+    au: useRef(null),
+    grow: useRef(null),
+    tnc: useRef(null),
+    // tnc2: useRef(null),
+    lifelines: useRef(null),
+    // satellite: useRef(null),
+    photo: useRef(null),
   };
 
   const handleVideoPlay = (videoKey) => {
@@ -20,201 +28,87 @@ const VideoPage = () => {
     }
   };
 
+  // Video list (with comments for excluded videos)
+  const videos = [
+    { ref: videoRefs.yasc, src: 'https://youtube.com/embed/yW_stTSn6HU', title: 'yasc' },
+    { ref: videoRefs.grow, src: 'https://youtube.com/embed/231F8qevHLg', title: 'regrow' },
+    { ref: videoRefs.au, src: 'https://youtube.com/embed/rpTR5hea4GE', title: 'WNT' },
+    { ref: videoRefs.wn, src: 'https://www.youtube.com/embed/bH25f59aO4I', title: 'WN' },
+    { ref: videoRefs.ox, src: 'https://youtube.com/embed/cxsvWjbjeWQ', title: 'OX' },
+    { ref: videoRefs.tnc, src: 'https://youtube.com/embed/5s2WVWY5fGU', title: 'TNC' },
+    // { ref: videoRefs.tnc2, src: 'https://youtube.com/embed/A1xf2RrvUOk', title: 'TNC2' },
+    { ref: videoRefs.lifelines, src: 'https://www.youtube.com/embed/l0LHeM6Khr0', title: 'lifelines' },
+    { ref: videoRefs.skate, src: 'https://youtube.com/embed/t6AaEDmw9ak', title: 'skate' },
+    // { ref: videoRefs.satellite, src: 'https://www.youtube.com/embed/7Lo8HUedzUs', title: 'satellite' },
+    { ref: videoRefs.photo, src: 'https://youtube.com/embed/1UZ2VDcOa9I', title: 'photo' },
+  ];
+
   return (
     <Layout>
       <Header />
       <section style={{ minHeight: '100vh' }}>
         <div className="app" style={{ alignItems: 'center', display: 'flex', flexDirection: 'column' }}>
-          
-        <div className="video-container" style={{ paddingTop: '160px'}}>
-            <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.yasc}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/yW_stTSn6HU?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="yasc"
-              ></iframe>
-            </div>
-            </div>
-            
-            
-            
-            <div className="video-container">
-            <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.au}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/rpTR5hea4GE?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="WNT"
-              ></iframe>
-            </div>
-            </div>
-  
-            
-            
-        
-        <div className="video-container">
-        <div className="video-wrapper">
-           <iframe
-                ref={videoRefs.grow}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/231F8qevHLg?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="regrow"
-              ></iframe>
-            </div>
-            </div>
-           
-
-
-        <div className="video-container">
-          <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.wn}
-                width="100%"
-                height="500"
-                src="https://www.youtube.com/embed/bH25f59aO4I?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="WN"
-              ></iframe>
-            </div>
+          <div
+            className="video-grid"
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(2, 1fr)',
+              gap: '30px',
+              width: '100%',
+              maxWidth: '1400px',
+              paddingTop: '160px',
+              paddingBottom: '50px',
+            }}
+          >
+            {videos.map((video, idx) => (
+              <div
+                key={idx}
+                className="video-wrapper"
+                style={{
+                  width: '100%',
+                  position: 'relative',
+                  paddingBottom: '56.25%', // 16:9 aspect ratio
+                  margin: '0', // desktop default
+                }}
+              >
+                <iframe
+                  ref={video.ref}
+                  src={`${video.src}?controls=1&rel=0&modestbranding=1`}
+                  title={video.title}
+                  frameBorder="0"
+                  allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  style={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    width: '100%',
+                    height: '100%',
+                  }}
+                ></iframe>
+              </div>
+            ))}
           </div>
-          
-          
-
-       {/* <div className="video-container">
-            <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.ox}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/cxsvWjbjeWQ?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="oceanxplorers"
-              ></iframe>
-            </div>
-          </div>*/}
-
-
-          
-
-          
-        <div className="video-container">
-            <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.tnc}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/5s2WVWY5fGU?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="TNC"
-              ></iframe>
-            </div>
-          </div>
-
-          {/*<div className="video-container">  
-            <div className="video-wrapper">
-            <iframe
-                ref={videoRefs.tnc2}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/A1xf2RrvUOk?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="TNC2"
-              ></iframe>
-            </div>
-          </div>*/}
-          
-          <div className="video-container">
-            <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.lifelines}
-                width="100%"
-                height="500"
-                src="https://www.youtube.com/embed/l0LHeM6Khr0?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="lifelines"
-              ></iframe>
-            </div>
-          </div>
-
-
-
-          
-          <div className="video-container">
-            <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.skate}
-                width="100%"
-                height="500"
-                src="https://www.youtube.com/embed/t6AaEDmw9ak?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="skate"
-              ></iframe>
-            </div>
-          </div>
-
-          
-
-          
-
-          {/*<div className="video-container">
-            <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.satellite}
-                width="100%"
-                height="500"
-                src="https://www.youtube.com/embed/7Lo8HUedzUs?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="satellite"
-              ></iframe>
-            </div>
-          </div>*/}
-
-          <div className="video-container">
-            <div className="video-wrapper">
-              <iframe
-                ref={videoRefs.photo}
-                width="100%"
-                height="500"
-                src="https://youtube.com/embed/1UZ2VDcOa9I?controls=1&rel=0&modestbranding=1"
-                //photo src="https://youtube.com/embed/RsK_VVAskJE?controls=1&rel=0&modestbranding=1"
-                frameBorder="0"
-                allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-                allowFullScreen
-                title="photo"
-              ></iframe>
-            </div>
-          </div>
-          
         </div>
       </section>
       <Footer />
+
+      {/* Mobile*/}
+      <style>
+        {`
+                @media (max-width: 768px) {
+              .video-wrapper {
+                margin: 20px 0;
+              }
+              .video-grid {
+                grid-template-columns: 1fr !important;
+                padding: 0 25px; /* side margins */
+              }
+            }
+
+
+        `}
+      </style>
     </Layout>
   );
 };
