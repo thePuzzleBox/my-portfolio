4e21b5626fcb0c096c51856d14cf42d8d33ce4ed
Jalen <jalensmith126@gmail.com>
2026-01-21 15:11:40 -0800

    bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -21,12 +21,12 @@ const AboutPage = () => (
           an extensive background in advocacy, Jalen sought to do meaningful work in visual storytelling. Through filming wildlife and teaching kids how to plant trees, he found he had a passion for nature and for sharing it. 
           
           
-          <br/><br/> Since then, Jalen's commitment to conservation, reforestation, and natural world storytelling has led him to work and film in communities worldwide. From sailing across Indonesia to planting trees across South America, he has lived in some
-          of the most remote regions on Earth, and has found himself in the presence of exceptionally diverse people, ecosystems, and alternative ways of life. As a result, he aims to not only share this wealth and knowledge of the
+          <br/><br/> Since then, Jalen's commitment to conservation and telling natural world stories has led him to work and film in communities worldwide. From sailing across Indonesia to planting trees across South America, he has found himself 
+          in some of the most remote regions on Earth, in the presence of exceptionally diverse people, ecosystems, and alternative ways of life. As a result, he aims to not only share this wealth and knowledge of the
           natural world, but through capturing its stories of preservation, he hopes others learn what it means to be a part of it.
 
 
-          <br /><br />Outside of being an adventurer, Jalen is an avid skateboarder, writer, undercover pianist, and urban forester who has ventured into the world of "seaforestation" and freediving.
+          <br /><br />Outside of being an adventurer, Jalen is an avid skateboarder, writer, undercover pianist, and urban forester who loves to share random ocean fun facts.
         </p>
       </div>
     </div>
