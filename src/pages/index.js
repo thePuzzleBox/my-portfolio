4d2fa21f01ad6970a4c4ad29e7157e098d792ecf
Jalen <jalensmith126@gmail.com>
2026-08-26 02:58:10 -0700

    upgrade

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Footnote from '../components/Footnote';
 
-import reel from '../assets/images/land.mp4';
+import reel from '../assets/images/landing.mp4';
 
 const IndexPage = () => (
   <Layout>
