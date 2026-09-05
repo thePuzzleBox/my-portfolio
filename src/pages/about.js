8326422fad3cfce21d5593fa9f1d1b3b0e6d4e58
Jalen <jalensmith126@gmail.com>
2026-09-05 03:11:28 -0700

    vid

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Footer from '../components/Footer2';
-import portrait from '../assets/images/imgg.jpg';
+import portrait from '../assets/images/img.jpg';
 
 const AboutPage = () => (
   <Layout>
