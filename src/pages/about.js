f2eb909ab6ae74c1c6041ad8e4e5e7ba05e5d06e
Jalen <jalensmith126@gmail.com>
2026-09-06 02:46:15 -0700

    edit

--- src/pages/about.js
+++ src/pages/about.js
@@ -26,11 +26,11 @@ const AboutPage = () => (
               a dream of filming wildlife. With little to no experience behind a camera, he knew that's where he needed to be. After living among untamed giants and experiencing a deeply connected
               way of life, he didn't just find what he was looking for. He returned having found what he wanted to dedicate his life to.
 
-              <br/><br/>Using visual storytelling to make seemingly distant worlds feel deeply personal, Jalen captures our complex relationships with nature and what they reveal about the human experience
+              <br/><br/>Drawn to stories in which people, place, and purpose shape one another, Jalen captures our complex relationships with nature and what they reveal about the human experience
               and our role in bettering it.
 
-              <br/><br/>Jalen’s portfolio spans documentary film and photography in expedition environments across land and sea. Based in Los Angeles, he currently freelances for OceanX and has worked
-              with organizations such as The Nature Conservancy. He earned his B.A. from Yale University after starting his education at community college.
+              <br/><br/>Jalen’s portfolio spans documentary film and photography in expedition environments across land and sea. Based in Los Angeles, he has worked with organizations such as The
+              Nature Conservancy and currently freelances for OceanX. He earned his B.A. from Yale University after starting his education at community college.
 
               <br/><br/>When he’s not in the field, he can usually be found sitting at a piano, skateboarding, planting trees big and small, or learning how to freedive.
 
